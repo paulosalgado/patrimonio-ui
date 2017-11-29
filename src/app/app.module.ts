02cdf68d4b0b8474f1e8b2afa74400f614398a16
Paulo José de Oliveira Salgado <paulo@technosoftware.com.br>
2017-11-29 14:49:07 -0200

    workshop aula 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { DataTableModule, ButtonModule, InputTextModule, InputMaskModule, CalendarModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ItemCadastroComponent } from './item-cadastro/item-cadastro.component';
 
+import { ItemService } from './item.service';
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ItemCadastroComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule, 
+    DataTableModule, ButtonModule, InputTextModule, InputMaskModule, CalendarModule, 
+    HttpClientModule, 
+    BrowserAnimationsModule, 
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  providers: [
+    ItemService
+  ],
+
+  bootstrap: [
+    AppComponent
+  ]
+
 })
+
 export class AppModule { }
